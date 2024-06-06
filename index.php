0d63a2aabf6fe70e76e45a5a8d6af40294cc58e3
Manuel Pérez Jurado <perezjuradomanuel@gmail.com>
2024-06-06 14:11:42 +0200

    added html structure and logo and created the navbar

--- index.php
+++ index.php
@@ -1,10 +1,39 @@
-<?php 
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header class="header">
+        <div class="contenedor">
+            <div class="logo">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logotipo">
+            </div>
+            <nav>
+                <a href="/wpgymfitness/nosotros">Nosotros</a>
+                <a href="/wpgymfitness/contacto">Contacto</a>
+                <a href="/wpgymfitness/blog">Blog</a>
+            </nav>
+        </div>
+    </header>
 
-while( have_posts()) : the_post();
+    <main>
+    <?php 
 
-the_title();
-the_content();
+    while( have_posts()) : the_post(); // start the loop and contact the DB of wp
 
-endwhile;
+    the_title(); // set the name of the page/post
+    the_content(); // set the content of the page/post
 
-?>
+    endwhile; // end the loop
+
+    ?>
+
+    </main>
+
+</body>
+</html>
+
+
