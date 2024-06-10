1447dc93c31e4284d66ac7e4c69b0fd7c131d547
Manuel Pérez Jurado <perezjuradomanuel@gmail.com>
2024-06-10 19:39:41 +0200

    changed the code to be  more reusable and easier to modify in the future

---
+++ template-parts/pagina.php
@@ -0,0 +1,14 @@
+<?php
+    while (have_posts()) : the_post(); // start the loop and contact the DB of wp
+
+        the_title("<h1 class='text-center primary-text'>", "</h1>"); // set the name of the page/post
+        if (has_post_thumbnail()) {
+            // we use this if to check if we have a featured image, if we have it we publish the image, if not we don't use this code saving execution time
+            the_post_thumbnail("full", array("class" => "featured-image"));
+            // set the featured image. As 1st param, u can set the size of the picture.
+            // As 2nd param with a associative array, u can set a class to modify with ur css
+        }
+        the_content(); // set the content of the page/post
+
+    endwhile; // end the loop
+?>
