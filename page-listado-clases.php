d37a2d6f089956f67408b4a1ccdda18aa3e5f1c2
Manuel Pérez Jurado <perezjuradomanuel@gmail.com>
2024-06-11 20:19:37 +0200

    rendering the WP_Query consults

--- page-listado-clases.php
+++ page-listado-clases.php
@@ -13,20 +13,23 @@
                     'post_type' => 'gymfitness_clases', // post type that we want to check
                 );
 
-                $clases = new WP_Query($args);
+                $clases = new WP_Query($args); // create queries to the WordPress database
 
                 while($clases->have_posts()) {
                         $clases->the_post();
-                    ?>
+            ?>
                         <li class="card">
-                            <?php 
-                                the_title();
-                            ?>
+                            <?php the_post_thumbnail(); ?>
+                            <div class="content">
+                                <a href="<?php the_permalink(); ?>">
+                                    <h3><?php the_title(); ?></h3>
+                                </a>
+                            </div>
                         </li>
-                <?php 
+            <?php 
                     }
-                    wp_reset_postdata();
-                ?>
+                    wp_reset_postdata(); // After looping through a separate query, this function restores the $post global to the current post in the main query.
+            ?>
             
         </ul>
     </main>
