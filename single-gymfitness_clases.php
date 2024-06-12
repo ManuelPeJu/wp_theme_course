f1fedb3c322164e2798d3df2200297f5e14a8b47
Manuel Pérez Jurado <perezjuradomanuel@gmail.com>
2024-06-12 17:55:36 +0200

    render individual pages

---
+++ single-gymfitness_clases.php
@@ -0,0 +1,8 @@
+<?php 
+    get_header();
+?>
+<h1>Solo visible para clases</h1>
+
+<?php 
+    get_footer();
+?>
---
+++ single.php
@@ -0,0 +1,8 @@
+<?php 
+    get_header();
+?>
+<h1>Hola amejo</h1>
+
+<?php 
+    get_footer();
+?>
