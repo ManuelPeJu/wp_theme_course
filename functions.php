ee30c009ef34673576c955220bc02b93f2545d4c
Manuel Pérez Jurado <perezjuradomanuel@gmail.com>
2024-06-12 18:59:23 +0200

    added widget support

--- functions.php
+++ functions.php
@@ -35,9 +35,17 @@
 
     add_action("wp_enqueue_scripts", "gymfitness_scripts_styles");
 
-
-
-
+    function gymfitness_widgets() {
+        register_sidebar(array(
+            'name'           => 'sidebar 1',
+            'id'             => 'sidebar_1',
+            'before_widget'  => '<div class="widget">',
+            'after_widget'   => '</div>',
+            'before_title'   => '<h3 class"text-center text-primary">',
+            'after_title'    => '</h3>',
+        ));
+    }
+    add_action("widgets_init", "gymfitness_widgets")
 
 
 
