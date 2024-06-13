0946d8961dadefb6fb94853ecb6c66641c02d108
Manuel Pérez Jurado <perezjuradomanuel@gmail.com>
2024-06-13 19:17:54 +0200

    starting blog page and structure

--- home.php
+++ home.php
@@ -1,12 +1,12 @@
-    <?php 
+<?php 
         get_header(); // get the header
     ?>
-    <h1>desde index</h1>
-    <main>
+    <main class="section container">
         <?php 
             while( have_posts()) : the_post(); // start the loop and contact the DB of wp
 
             the_title(); // set the name of the page/post
+
             the_content(); // set the content of the page/post
 
             endwhile; // end the loop
