2fad00360839729c65c805f362775408934be2c8
Manuel Pérez Jurado <perezjuradomanuel@gmail.com>
2024-06-13 21:19:51 +0200

    author template

--- author.php
+++ author.php
@@ -1,1 +1,27 @@
-<h1>que dice el tio desde author.php</h1>
+<?php
+    get_header(); // get the header
+?>
+
+<main class="section container">
+    <?php 
+        $author = get_queried_object(); //get the object that WP are consulting to the DB
+    ?>
+    <h2 class="primary-text text-center">Autor: <?php echo $author->data->display_name; ?></h2>
+    <p class="text-center">
+        <?php 
+            echo get_the_author_meta("description", $author->data->ID);
+        ?>
+    </p>
+    <ul class="grid-list">
+        <?php
+            while (have_posts()) {
+                the_post();
+                get_template_part("template-parts/blog");
+            }
+        ?>
+    </ul>
+</main>
+
+<?php
+    get_footer(); // get the footer
+?>
