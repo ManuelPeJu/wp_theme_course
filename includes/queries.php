8c94f1d4aff18699d33a8618ea9f901817fa3d57
Manuel Pérez Jurado <perezjuradomanuel@gmail.com>
2024-06-17 19:49:04 +0200

    render classes on home page and refactoring code

---
+++ includes/queries.php
@@ -0,0 +1,48 @@
+<?php
+
+function gymfitness_lista_clases($cantidad = -1) // setting $cantidad = -1, make in wp_query is getting all the results
+{
+?>
+
+    <ul class="grid-list">
+        <?php
+        $args = array(
+            'post_type' => 'gymfitness_clases', // post type that we want to check
+            'posts_per_page' => $cantidad,// how many post we want in a single page
+        );
+
+        $clases = new WP_Query($args); // create queries to the WordPress database
+
+        while ($clases->have_posts()) {
+            $clases->the_post();
+        ?>
+            <li class="card">
+                <?php the_post_thumbnail(); ?>
+                <div class="content">
+                    <a href="<?php the_permalink(); ?>"> <!-- Return the direct permalink to our classes -->
+                        <h3>
+                            <?php the_title(); ?>
+                        </h3>
+                    </a>
+                    <?php
+                    /* get_field(''); obtains data from a field and stores it in a variable */
+                    $hora_inicio = get_field('hora_inicio');
+                    $hora_fin = get_field('hora_fin');
+                    ?>
+                    <p>
+                        <!-- the_field(''); obtains the data collected on the variable that used get_field(''); -->
+                        <?php the_field('dias_clase'); ?> - <?php echo $hora_inicio . " a " . $hora_fin ?>
+                    </p>
+                </div>
+            </li>
+        <?php
+        }
+        wp_reset_postdata(); // After looping through a separate query, this function restores the $post global to the current post in the main query.
+        ?>
+
+    </ul>
+
+<?php
+}
+
+?>
