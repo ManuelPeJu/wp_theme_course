2ce3776c7072cb86537624f61362ad78cba6fc5e
Manuel Pérez Jurado <perezjuradomanuel@gmail.com>
2024-06-17 18:56:54 +0200

    adding the rest of the areas

--- front-page.php
+++ front-page.php
@@ -16,11 +16,42 @@
         <?php
             $area_1 =  get_field("area_1");
             $imagen = $area_1["imagen"]["sizes"]["medium_large"];
-            $texto = $area_1["texto"]
+            $texto = $area_1["texto"];
         ?>
         <img src="<?php echo esc_attr($imagen); ?>" alt="<?php echo esc_attr($texto) ?>">
         <p><?php echo esc_html($texto); ?></p>
     </div>
+
+    <div class="area">
+        <?php
+            $area_2 =  get_field("area_2");
+            $imagen = $area_2["imagen"]["sizes"]["medium_large"];
+            $texto = $area_2["texto"];
+        ?>
+        <img src="<?php echo esc_attr($imagen); ?>" alt="<?php echo esc_attr($texto) ?>">
+        <p><?php echo esc_html($texto); ?></p>
+    </div>
+
+    <div class="area">
+        <?php
+            $area_3 =  get_field("area_3");
+            $imagen = $area_3["imagen"]["sizes"]["medium_large"];
+            $texto = $area_3["texto"];
+        ?>
+        <img src="<?php echo esc_attr($imagen); ?>" alt="<?php echo esc_attr($texto) ?>">
+        <p><?php echo esc_html($texto); ?></p>
+    </div>
+
+    <div class="area">
+        <?php
+            $area_4 =  get_field("area_4");
+            $imagen = $area_4["imagen"]["sizes"]["medium_large"];
+            $texto = $area_4["texto"];
+        ?>
+        <img src="<?php echo esc_attr($imagen); ?>" alt="<?php echo esc_attr($texto) ?>">
+        <p><?php echo esc_html($texto); ?></p>
+    </div>
+
 </section>
 
 <main class="container section">
