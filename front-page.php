fd90e8cef86191a6e107e75b9c25b722ac0e2009
Manuel Pérez Jurado <perezjuradomanuel@gmail.com>
2024-06-17 17:44:19 +0200

    welcome section

--- front-page.php
+++ front-page.php
@@ -2,8 +2,17 @@
     get_header();
 ?>
 
+<section class="welcome section container text-center">
+    <h2 class="primary-text">
+        <?php the_field("encabezado_bienvenida");?>
+    </h2>
+    <p>
+        <?php the_field("texto_bienvenida");?>
+    </p>
+</section>
+
 <main class="container section">
-    <h1>desde página de inicio</h1>
+    
 </main>
 
 <?php 
