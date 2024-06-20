042037e07b8b6aed14d25c72b09e7270f5f0fab3
Manuel Pérez Jurado <perezjuradomanuel@gmail.com>
2024-06-20 18:57:44 +0200

    rendering comments

--- single.php
+++ single.php
@@ -10,6 +10,25 @@
         <div class="comments focus-content text-center">
             <?php 
                 comment_form();
+                ?>
+
+                <h3 class="text-center primary-text comments">Comentarios</h3>
+                <ul class="comments-list">
+                    <?php 
+                        $comentarios = get_comments(array(
+                            'post_id' => $post -> ID,
+                            'status' => 'approve',
+
+                        ));
+                        wp_list_comments(array(
+                            'per_page' => 10,
+                            'reverse_top_level' => false,
+                        ), $comentarios)
+                    
+                    ?>
+                </ul>
+
+                <?php
             ?>
         </div>
     </main>
