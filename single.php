befd0bc7afb9dde154a8f48330590c46ebafa631
Manuel Pérez Jurado <perezjuradomanuel@gmail.com>
2024-06-20 18:39:29 +0200

    creating, render and css comments form

--- single.php
+++ single.php
@@ -6,6 +6,12 @@
         <?php 
             get_template_part("template-parts/post");
         ?>
+
+        <div class="comments focus-content text-center">
+            <?php 
+                comment_form();
+            ?>
+        </div>
     </main>
 
 <?php 
